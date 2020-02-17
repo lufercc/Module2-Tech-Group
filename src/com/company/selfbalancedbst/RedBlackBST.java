489a48464fb04c6efba309454c23956f1da7b4c5
Luis Cachi <lufer.lfcc@gmail.com>
2020-02-17 16:12:28 -0400

    Rigth rotate

--- src/com/company/selfbalancedbst/RedBlackBST.java
+++ src/com/company/selfbalancedbst/RedBlackBST.java
@@ -52,6 +52,26 @@ public class RedBlackBST {
         return aux;
     }
 
+    protected ColorNode rightRotate(ColorNode node) {
+        ColorNode parent = node.getParent();
+        ColorNode aux = node.getLeft();
+        aux.setParent(parent);
+        node.setLeft(aux.getRight());
+        aux.setRight(node);
+        node.setParent(aux);
+        node.getLeft().setParent(node);
+        if (parent != null) {
+            if (parent.getRight() == node)  {
+                parent.setRight(aux);
+            } else {
+                parent.setLeft(aux);
+            }
+        } else {
+            root = aux;
+        }
+        return aux;
+    }
+
     private void fixTree(ColorNode node) {
 
         root.setColor(Color.BLACK);
